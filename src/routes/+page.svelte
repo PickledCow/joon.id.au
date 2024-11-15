bc7d8d8a462d3f61897b943ed5b6ed19ee8012d6
George Fischer <me@george.id.au>
2024-11-15 23:16:47 +1100

    thing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<div class="w-96 h-96 absolute bg"></div>
+<h1 class="text-9xl font-semibold name rotate-90 translate-y-20 translate-x-3">
+  Joon Suh
+</h1>
+
+<style lang="postcss">
+  .name {
+    background: url(./orb.svg) center / cover;
+    width: min-content;
+    line-height: 0.85em;
+    color: transparent;
+    background-clip: text;
+    position: relative;
+  }
+
+  .bg {
+    background: url(./orb.svg) center / cover;
+  }
+</style>
