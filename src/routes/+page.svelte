41f41ead72363066825279c5efa7180a8ce1b9d4
George Fischer <me@george.id.au>
2024-11-15 23:23:32 +1100

    ya

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,16 @@
-<div class="w-96 h-96 absolute bg"></div>
-<h1 class="text-9xl font-semibold name rotate-90 translate-y-20 translate-x-3">
+<div class="city inset-0 absolute grayscale blur"></div>
+<div class="w-96 h-96 absolute bg saturate-50"></div>
+
+<h1
+  class="text-9xl font-semibold name rotate-90 brightness-150 translate-y-20 translate-x-2"
+>
   Joon Suh
 </h1>
 
 <style lang="postcss">
+  .city {
+    background: url(../lib/city.avif) center / cover;
+  }
   .name {
     background: url(./orb.svg) center / cover;
     width: min-content;
